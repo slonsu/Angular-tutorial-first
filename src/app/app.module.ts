888b0b6d2cfcc1aff9066780dfbf491efda38fca
onsu <jeiranashvili@gmail.com>
2019-07-04 21:21:48 +0400

    feat(Angular-Tuturial-First): Finish tutorial

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { RouterModule } from '@angular/router';
+import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { CartComponent } from './cart/cart.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TopBarComponent,
+    ProductListComponent,
+    ProductAlertsComponent,
+    ProductDetailsComponent,
+    CartComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path: '', component: ProductListComponent},
+      {path: 'products/:productId', component: ProductDetailsComponent},
+      {path: 'cart', component: CartComponent}
+    ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,23 @@
+import {Component} from '@angular/core';
+import {products} from '../products';
+
+@Component({
+    selector: 'app-product-list',
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./product-list.component.scss']
+})
+
+export class ProductListComponent {
+    products = products;
+
+    constructor() {
+    }
+
+    share() {
+        window.alert('the product has been shared');
+    }
+
+    onNotify() {
+        window.alert('You will be gnomed');
+    }
+}
