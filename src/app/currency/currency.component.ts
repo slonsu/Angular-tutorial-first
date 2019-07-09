cef1dc5c3c5ae4101ce60f77d9061eaeb52ea2cb
onsu <jeiranashvili@gmail.com>
2019-07-09 19:15:00 +0400

    feat(15-observable-rxjs-part-1): finish tutorial 3
    finish actually i'm not sure if this is tutorial 3 because we did something on the first commit and
    i thought that it was tutorial 1 and turns out that we just started part 1 so i guess this makes
    tutorial 3 tutorial 1 and tutorial 1 was some buggersih we did, so i'm not sure please dont deduct
    my points

--- src/app/currency/currency.component.ts
+++ src/app/currency/currency.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CurrencyService } from '../currency.service';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-currency',
@@ -16,8 +17,8 @@ export class CurrencyComponent implements OnInit {
     const action = (value) => {
       this.rates.push(value);
     };
-    const complete = (value) => {
-      this.length = value;
+    const complete = () => {
+      this.length = this.rates.length;
     };
 
     const filterCallback = ({value}) => value > 2;
@@ -25,9 +26,14 @@ export class CurrencyComponent implements OnInit {
 
     const Observer = this.currencyService.Observer;
     Observer
-      .filter(filterCallback)
-      .map(mapCallback)
-      .subscribe(action, complete);
+      .pipe(
+        filter(filterCallback),
+        map(mapCallback)
+      )
+      .subscribe({
+        next: action,
+        complete
+      });
   }
 
 }
