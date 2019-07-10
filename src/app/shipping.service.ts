c7b209819b3c139aec9916e220f7cae7fb278777
onsu <jeiranashvili@gmail.com>
2019-07-10 19:00:15 +0400

    feat(20-di-part-3): finish tutorial 3

--- src/app/shipping.service.ts
+++ src/app/shipping.service.ts
@@ -1,13 +1,24 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-@Injectable()
+@Injectable({
+  providedIn: 'root'
+})
 
 export class ShippingService {
+  type;
 
   constructor(private http: HttpClient) { }
 
   getShippingCosts() {
     return this.http.get('/assets/shipping.json');
+  }
+
+  setShipping(value) {
+    this.type = value;
+  }
+
+  getType() {
+    return this.type;
   }
 }
