40278ce56b992d314267d9a5fb4acd03d4aae5e2
onsu <jeiranashvili@gmail.com>
2019-07-10 20:39:58 +0400

    feat(4-Obsarvables-tasks): Work in progress

--- src/app/currency/currency.component.ts
+++ src/app/currency/currency.component.ts
@@ -12,6 +12,10 @@ export class CurrencyComponent implements OnInit {
   length = 0;
 
   constructor(private currencyService: CurrencyService) { }
+  currency1;
+  currency2;
+  rate1;
+  rate2;
 
   ngOnInit() {
     const action = (value) => {
@@ -21,7 +25,7 @@ export class CurrencyComponent implements OnInit {
       this.length = this.rates.length;
     };
 
-    const filterCallback = ({value}) => value > 2;
+    const filterCallback = ({value}) => value > 0;
     const mapCallback = ({rates}) => {
       const currency = Object.keys(rates)[0];
       const value = rates[currency];
@@ -44,4 +48,31 @@ export class CurrencyComponent implements OnInit {
       });
   }
 
+  selectCurrency1(value) {
+    this.rate1 = value;
+    this.updateCurrency2();
+  }
+
+  selectCurrency2(value) {
+    this.rate2 = value;
+    this.updateCurrency1();
+  }
+
+  setCurrency1(value) {
+    this.currency1 = value;
+    this.updateCurrency2();
+  }
+
+  setCurrency2(value) {
+    this.currency2 = value;
+    this.updateCurrency1();
+  }
+
+  updateCurrency2() {
+    this.currency2 = this.currency1 * this.rate1;
+  }
+
+  updateCurrency1() {
+    this.currency1 = this.currency2 * this.rate1;
+  }
 }
