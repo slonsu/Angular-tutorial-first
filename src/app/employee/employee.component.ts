207e8c72f54e06125a6c3974839fff4f319c1574
onsu <jeiranashvili@gmail.com>
2019-07-16 00:04:30 +0400

    feat(26-animations): Finish tasks 1 and 2

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,13 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { EmployeesService } from '../employees.service';
+import { state, trigger, style, transition, animate, keyframes } from '@angular/animations';
 
 @Component({
   selector: 'app-employee',
   templateUrl: './employee.component.html',
-  styleUrls: ['./employee.component.scss']
+  styleUrls: ['./employee.component.scss'],
+  animations: [trigger('flyIn', [
+    state('open', style({
+      opacity: 1
+    })),
+    transition('* => open', [
+      animate('1s', keyframes([
+        style({opacity: 0, offset: 0}),
+        style({opacity: 1, offset: '1s'})
+      ]))
+    ]),
+    transition('open => *', [
+      animate('1s', keyframes([
+        style({opacity: 1, offset: 0}),
+        style({opacity: 0, offset: '1s'})
+      ]))
+    ])
+  ])]
 })
 export class EmployeeComponent implements OnInit {
+  popup = false;
   employee$;
   id;
 
@@ -23,5 +42,17 @@ export class EmployeeComponent implements OnInit {
 
   delete() {
     this.employeesService.deleteEmployee(this.id).subscribe();
+    this.hideDialog();
+  }
+
+  close() {
+  }
+
+  showDialog() {
+    this.popup = true;
+  }
+
+  hideDialog() {
+    this.popup = false;
   }
 }
